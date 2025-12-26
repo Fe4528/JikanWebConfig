5d63c964a178a10dd3e314b1ee88ca288cc042ef
Fe4528 <yevgenysepato000@gmail.com>
2025-12-26 09:28:42 +0800

    auth test

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('node:path');
 const { loadEnvFile } = require('node:process'); loadEnvFile('.env');
+const undici = require('undici');
 
 const app = express();
-app.use('/asset',express.static(path.join(__dirname, 'asset')));
+app.use('/asset', express.static(path.join(__dirname, 'asset')));
 
 app.get('/', (request, response) => {
 	return response.sendFile(path.join(__dirname, 'pages', 'login.html'));
@@ -11,6 +12,92 @@ app.get('/', (request, response) => {
 
 app.get('/test', (request, response) => {
 	return response.sendFile(path.join(__dirname, 'pages', 'test.html'));
+});
+
+app.get('/auth', async (request, response) => {
+	const clientId = process.env.CLIENT_ID;
+	const redirectUri = encodeURIComponent(process.env.REDIRECT_URI);
+
+	response.redirect(`https://discord.com/oauth2/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=identify+guilds`);
+});
+
+/*
+app.get('/auth/callback', async (request, response) => {
+	const code = request.query.code;
+
+	if (code == undefined) return response.status(400).send('Invalid content');
+
+	//response.write('Wait...');
+
+	try {
+		const res_data = await undici.request('https://discord.com/api/oauth2/token', {
+			method: 'POST',
+			body: new URLSearchParams({
+				client_id: process.env.CLIENT_ID,
+				client_secret: process.env.CLIENT_SECRET,
+				code,
+				grant_type: 'authorization_code',
+				redirect_uri: `${encodeURIComponent(process.env.REDIRECT_URI)}`,
+				scope: 'identify guilds',
+			}),
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
+		});
+
+		const auth_data = await res_data.body.json();
+		if (auth_data.access_token == undefined) throw new Error('No access token');
+
+		console.log(auth_data);
+		// auth data should be saved server side
+	} catch (error) {
+		return response.status(401).send('401 - Unauthorized');
+	}
+
+	response.send('Redirecting...')
+	// code is for getting an access token from discord	
+	return response.redirect('/dashboard');
+});
+*/
+
+app.get('/auth/callback', async (request, response) => {
+	const code = request.query.code;
+	if (!code) return response.status(400).send('Invalid code');
+
+	try {
+		const res_data = await undici.request('https://discord.com/api/oauth2/token', {
+			method: 'POST',
+			body: new URLSearchParams({
+				client_id: process.env.CLIENT_ID,
+				client_secret: process.env.CLIENT_SECRET,
+				grant_type: 'authorization_code',
+				code,
+				redirect_uri: process.env.REDIRECT_URI,
+			}).toString(),
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
+		});
+
+		const auth_data = await res_data.body.json();
+
+		if (!auth_data.access_token) {
+			console.error(auth_data);
+			return response.status(401).send('Fail to authenticate');
+		}
+
+		console.log(auth_data);
+
+		return response.redirect('/dashboard');
+	} catch (err) {
+		console.error(err);
+		return response.status(500).send('Authentication error');
+	}
+});
+
+
+app.get('/dashboard', (request, response) => {
+	return response.sendFile(path.join(__dirname, 'pages', 'dashboard.html'));
 });
 
 app.listen(process.env.WEB_PORT, () => console.log(`App listening at http://localhost:${process.env.WEB_PORT}`));
